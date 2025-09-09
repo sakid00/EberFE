b2eba153f6b7b4706ac46c618be50545774c65a4
sidikfahmi <fahmi.sidik@nanovest.io>
2025-09-09 19:05:51 +0700

    update config and translation

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,28 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_IMAGE_BASE_URL as string,
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'eber-api.agepedia.info',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'eber-api.agepedia.infouploads',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
