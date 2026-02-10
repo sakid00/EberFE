dc577f2207b761aa839e2409f28d50a8ed2754e0
reinardricky <reinardricky@gmail.com>
2026-02-10 10:24:56 +0700

    Add new facivon.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,9 +92,9 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: '/logo.png',
-    shortcut: '/logo.png',
-    apple: '/logo.png',
+    icon: '/favicon.png',
+    shortcut: '/favicon.png',
+    apple: '/favicon.png',
   },
 };
 
